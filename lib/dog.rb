0c32cc5f466786d34f2edca5c42f89968c0184a7
brigitteorina <brigitteorina@gmail.com>
2023-05-17 15:48:54 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,31 @@
 class Dog
+    def name
+      @name
+    end
+  
+    def name=(new_name)
+      @name = new_name
+    end
+  
+    def breed
+      @breed
+    end
+  
+    def breed=(new_breed)
+      @breed = new_breed
+    end
+  end
+  
 
-end
+  dog = Dog.new
+dog.name = "Pap"
+dog.breed = "Arshier"
+
+puts dog.name  # Output: Pap
+puts dog.breed # Output: Arshier
+
+dog.name = "bob"
+dog.breed = "jersey"
+
+puts dog.name  # Output: bob
+puts dog.breed # Output: jersey
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,31 @@
 class Person
+    def name
+      @name
+    end
+  
+    def name=(new_name)
+      @name = new_name
+    end
+  
+    def job
+      @job
+    end
+  
+    def job=(new_job)
+      @job = new_job
+    end
+  end
+  
 
-end
+  person = Person.new
+person.name = "John Doe"
+person.job = "Software Engineer"
+
+puts person.name  # Output: John Doe
+puts person.job   # Output: Software Engineer
+
+person.name = "Jane Smith"
+person.job = "Data Scientist"
+
+puts person.name  # Output: Jane Smith
+puts person.job   # Output: Data Scientist
